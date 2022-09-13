19e6c27a28b5e28d08ae9f0257b438c2093630ff
TrishnaShelly <105371850+TrishnaShelly@users.noreply.github.com>
2022-09-13 20:48:43 +0530

    kadern Algorithm

---
+++ kadensAlgo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int kadane(int arr[], int n)
+{
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currentSum += arr[i];
+        if (currentSum < 0)
+            currentSum = 0;
+        maxSum = max(maxSum,currentSum);
+        
+    }
+    return maxSum;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int wrapsum;
+    int nonWrapSum;
+
+    nonWrapSum= kadane(arr,n);
+    int totalSum =0;
+    for(int i =0;i<n;i++){
+        totalSum +=arr[i];
+        arr[i]=-arr[i];
+    }
+
+    wrapsum = totalSum + kadane(arr,n);
+
+    cout << max(wrapsum,nonWrapSum);
+    return 0;
+}
